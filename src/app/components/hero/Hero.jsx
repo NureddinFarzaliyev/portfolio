fa4d46603611a9323e27a7c81077755f4bd7d826
NureddinFarzaliyev <nureddin.fa@gmail.com>
2024-11-14 10:42:07 +0400

    hero section

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BackgroundBeams } from '../ui/background-beams'
-import { displayFont, textFont } from '@/app/utils/FontProvider'
+import { displayFont} from '@/app/utils/FontProvider'
+
+import { FaReact, FaNodeJs, FaBootstrap, FaPython} from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiTypescript, SiTailwindcss, SiExpress, SiMongodb } from "react-icons/si";
 
 
 function Hero() {
   return (
     <div className={`h-dvh flex flex-col justify-center items-center text-center gap-3`}>
         {/* <div> */}
-            <h1 className={`font-bold text-6xl ${displayFont.className}`}>Nureddin Farzaliyev</h1>
+            <h1 className={`font-bold px-5 text-5xl sm:text-7xl tracking-tight ${displayFont.className} text-glow`}>Nureddin Farzaliyev</h1>
             <p className='text-[rgba(255,255,255,0.5)] hover:text-[rgba(255,255,255,1)] transition-colors cursor-pointer'>Frontend / MERN-Stack Developer</p>
             <div>
-                skills
+                <FaPython />
             </div>
         {/* </div> */}
         <BackgroundBeams className="pointer-events-none"/>
