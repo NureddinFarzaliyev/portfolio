0faeff64a2b847a62369323664d545e8165fe3b1
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-03-01 10:23:25 +0400

    added new skills

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -7,15 +7,13 @@ import { HeroHighlight } from '../ui/hero-highlight'
 function Hero() {
   return (
     <div className={`h-dvh flex flex-col justify-center items-center text-center gap-6`}>
-      <HeroHighlight/>
+      <HeroHighlight />
       <IconSet linkSet={socialLinkSet} />
-      <h1 className={`font-bold px-5 py-1 text-4xl sm:text-7xl tracking-tight ${displayFont.className} text-glow`}>NUREDDIN<br/>FARZALIYEV</h1>
+      <h1 className={`font-bold px-5 py-1 text-4xl sm:text-7xl tracking-tight ${displayFont.className} text-glow`}>NUREDDIN<br />FARZALIYEV</h1>
       <div>
         <IconSet iconSet={skillIcons} />
         <p className='text-[rgba(255,255,255,0.2)] hover:text-[rgba(255,255,255,1)] text-md md:text-xl transition-all cursor-pointer text-hover-glow mt-4 relative z-40'>Frontend / MERN-Stack Developer</p>
       </div>
-
-
     </div>
   )
 }
