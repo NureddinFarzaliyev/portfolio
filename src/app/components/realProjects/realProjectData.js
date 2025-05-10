cdf333c2c412fb6de40f49caec52ec9a97a48f78
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-05-10 10:53:16 +0400

    real projects section

---
+++ src/app/components/realProjects/realProjectData.js
@@ -0,0 +1,25 @@
+const realProjectData = [
+  {
+    title: "Brendmoto API",
+    date: "02.25",
+    description: "An API for a complex commercial warehouse management system with a lot of different functionalities.",
+    skills: ['js', 'mongo', 'express', 'node', 'jwt'],
+    image: '/projectThumbnails/brendmoto_api.png',
+  },
+  {
+    title: "Matrix Forms",
+    date: "02.25",
+    description: "Full-Stack application to create forms and manage responses in a dashboard which includes functionalities such as creating different accounts with lower permissions, extracting form responses as an Xlsx file or customizing alert messages in a rich text editor.",
+    skills: ['mongo', 'express', 'react', 'node', 'bootstrap'],
+    image: '/projectThumbnails/matrixforms.png',
+  },
+  {
+    title: "BDU TGT",
+    date: "04.25",
+    description: "A website for the students of BDU (BSU) youth foundation which includes features such as registration for the organization or events, following latest news and projects of the organization.",
+    skills: ['react', 'tailwind',],
+    image: '/projectThumbnails/bdutgt.png',
+  },
+]
+
+export default realProjectData
--- src/app/page.js
+++ src/app/page.js
@@ -7,23 +7,25 @@ import TimelineComponent from "./components/timeline/TimelineComponent";
 import Contact from "./components/contact/Contact";
 import Head from "next/head";
 import Script from "next/script";
+import RealProjects from "./components/realProjects/RealProjects";
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <link rel="stylesheet" href="https://s.pageclip.co/v1/pageclip.css" media="screen" />
-    </Head>
-    <Script src="https://s.pageclip.co/v1/pageclip.js" charset="utf-8" />
-    <TracingBeam className={`text-white ${textFont.className}`}>
-    {/* <div className={`text-white ${textFont.className}`}> */}
-      {/* <Tracing /> */}
-      <Hero/>
-      <Projects/>
-      <TimelineComponent/>
-      <Contact/>
-    {/* </div> */}
-    </TracingBeam>
-  </>
+      <Head>
+        <link rel="stylesheet" href="https://s.pageclip.co/v1/pageclip.css" media="screen" />
+      </Head>
+      <Script src="https://s.pageclip.co/v1/pageclip.js" charset="utf-8" />
+      <TracingBeam className={`text-white ${textFont.className}`}>
+        {/* <div className={`text-white ${textFont.className}`}> */}
+        {/* <Tracing /> */}
+        <Hero />
+        <Projects />
+        <RealProjects />
+        <TimelineComponent />
+        <Contact />
+        {/* </div> */}
+      </TracingBeam>
+    </>
   );
 }
