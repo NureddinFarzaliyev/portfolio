9507efb595eb5a102230fb919330c12344cab526
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-05-20 10:16:58 +0400

    added next.js icon

--- src/app/utils/IconProvider.jsx
+++ src/app/utils/IconProvider.jsx
@@ -1,9 +1,10 @@
 import { FaReact, FaNodeJs, FaBootstrap, FaPython, FaYoutube, FaInstagram, FaGithub, FaLinkedin, FaCoffee, FaGitAlt, FaLinux, FaFigma, FaHtml5, FaCss3, FaDev, FaEnvelope } from "react-icons/fa";
-import { SiTailwindcss, SiExpress, SiMongodb, SiRedux, SiSupabase } from "react-icons/si";
+import { SiTailwindcss, SiExpress, SiMongodb, SiRedux, SiSupabase, SiNextdotjs } from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io";
 import { TbBrandTypescript } from "react-icons/tb";
 
-export const skillIcons = [FaPython, FaGitAlt, FaLinux, FaFigma, FaHtml5, FaCss3, SiTailwindcss, FaBootstrap, IoLogoJavascript, TbBrandTypescript, FaReact, FaNodeJs, SiExpress, SiMongodb, SiRedux, SiSupabase]
+export const skillIcons = [FaPython, FaGitAlt, FaLinux, FaFigma, FaHtml5, FaCss3, SiTailwindcss,
+    FaBootstrap, IoLogoJavascript, TbBrandTypescript, FaReact, SiNextdotjs, FaNodeJs, SiExpress, SiMongodb, SiRedux, SiSupabase]
 
 
 export const socialLinkSet = {
