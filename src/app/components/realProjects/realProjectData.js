818ba2a4f3838b3b08161653b678e55f1a969568
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-09-26 16:28:48 +0400

    update

--- src/app/components/realProjects/realProjectData.js
+++ src/app/components/realProjects/realProjectData.js
@@ -2,24 +2,44 @@ const realProjectData = [
   {
     title: "Brendmoto API",
     date: "02.25",
-    description: "An API for a complex commercial warehouse management system with a lot of different functionalities.",
-    skills: ['js', 'mongo', 'express', 'node', 'jwt'],
-    image: '/projectThumbnails/brendmoto_api.png',
+    description:
+      "A REST API for a complex commercial warehouse management system with a lot of different functionalities.",
+    skills: ["js", "mongo", "express", "node", "jwt"],
+    image: "/projectThumbnails/brendmoto_api.png",
   },
+
   {
     title: "Matrix Forms",
     date: "02.25",
-    description: "Full-Stack application to create forms and manage responses in a dashboard which includes functionalities such as creating different accounts with lower permissions, extracting form responses as an Xlsx file or customizing alert messages in a rich text editor.",
-    skills: ['mongo', 'express', 'react', 'node', 'bootstrap'],
-    image: '/projectThumbnails/matrixforms.png',
+    description:
+      "Full-Stack application to create forms and manage responses in a dashboard which includes functionalities such as creating different accounts with lower permissions, extracting form responses as an Xlsx file or customizing alert messages in a rich text editor.",
+    skills: ["mongo", "express", "react", "node", "bootstrap"],
+    image: "/projectThumbnails/matrixforms.png",
   },
   {
     title: "BDU TGT",
     date: "04.25",
-    description: "A website for the students of BDU (BSU) youth foundation which includes features such as registration for the organization or events, following latest news and projects of the organization.",
-    skills: ['react', 'tailwind',],
-    image: '/projectThumbnails/bdutgt.png',
+    description:
+      "A website for the students of BDU (BSU) youth foundation which includes features such as registration for the organization or events, following latest news and projects of the organization.",
+    skills: ["react", "tailwind"],
+    image: "/projectThumbnails/bdutgt.png",
   },
-]
+  {
+    title: "Lectura",
+    date: "08.25",
+    description:
+      "A comprehensive class management system with extensive features for teachers.",
+    skills: ["ts", "react", "tailwind", "supabase"],
+    image: "/projectThumbnails/lectura.png",
+    live: "https://lectura-az.netlify.app/",
+  },
+  {
+    title: "Brendmoto WEB API",
+    date: "09.25",
+    description: "A complete REST API for the e-commerce website.",
+    skills: ["ts", "mongo", "express", "node", "jwt"],
+    image: "/projectThumbnails/brendmoto_api.png",
+  },
+];
 
-export default realProjectData
+export default realProjectData;
--- src/app/utils/IconProvider.jsx
+++ src/app/utils/IconProvider.jsx
@@ -1,18 +1,56 @@
-import { FaReact, FaNodeJs, FaBootstrap, FaPython, FaYoutube, FaInstagram, FaGithub, FaLinkedin, FaCoffee, FaGitAlt, FaLinux, FaFigma, FaHtml5, FaCss3, FaDev, FaEnvelope } from "react-icons/fa";
-import { SiTailwindcss, SiExpress, SiMongodb, SiRedux, SiSupabase, SiNextdotjs } from "react-icons/si";
+import {
+  FaReact,
+  FaNodeJs,
+  FaBootstrap,
+  FaPython,
+  FaYoutube,
+  FaGithub,
+  FaLinkedin,
+  FaCoffee,
+  FaGitAlt,
+  FaLinux,
+  FaFigma,
+  FaHtml5,
+  FaCss3,
+  FaDev,
+  FaEnvelope,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiExpress,
+  SiMongodb,
+  SiRedux,
+  SiSupabase,
+  SiNextdotjs,
+} from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io";
 import { TbBrandTypescript } from "react-icons/tb";
 
-export const skillIcons = [FaPython, FaGitAlt, FaLinux, FaFigma, FaHtml5, FaCss3, SiTailwindcss,
-    FaBootstrap, IoLogoJavascript, TbBrandTypescript, FaReact, SiNextdotjs, FaNodeJs, SiExpress, SiMongodb, SiRedux, SiSupabase]
-
+export const skillIcons = [
+  FaPython,
+  FaGitAlt,
+  FaLinux,
+  FaFigma,
+  FaHtml5,
+  FaCss3,
+  SiTailwindcss,
+  FaBootstrap,
+  IoLogoJavascript,
+  TbBrandTypescript,
+  FaReact,
+  SiNextdotjs,
+  FaNodeJs,
+  SiExpress,
+  SiMongodb,
+  SiRedux,
+  SiSupabase,
+];
 
 export const socialLinkSet = {
-    'https://www.instagram.com/nureddinfarzaliyev': FaInstagram,
-    'https://www.youtube.com/@nureddinfarzaliyev': FaYoutube,
-    'https://www.github.com/nureddinfarzaliyev': FaGithub,
-    'https://www.linkedin.com/in/nureddinfarzaliyev': FaLinkedin,
-    'https://www.kofe.al/farzaliyev': FaCoffee,
-    'https://dev.to/nureddinfarzaliyev': FaDev,
-    'nureddin.fa@gmail.com': FaEnvelope
-}
+  "https://www.youtube.com/@nureddinfarzaliyev": FaYoutube,
+  "https://www.github.com/nureddinfarzaliyev": FaGithub,
+  "https://www.linkedin.com/in/nureddinfarzaliyev": FaLinkedin,
+  "https://www.kofe.al/farzaliyev": FaCoffee,
+  "https://dev.to/nureddinfarzaliyev": FaDev,
+  "nureddin.fa@gmail.com": FaEnvelope,
+};
